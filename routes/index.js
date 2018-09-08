0f8d2c11b9287ba3811216f97775f6572a7faca8
Imen Mahjoubi <imenmahjoubi@Imens-MacBook-Pro.local>
2018-09-08 14:40:08 -0700

    small test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'My Nutri-Advisor' });
 });
 
 module.exports = router;
