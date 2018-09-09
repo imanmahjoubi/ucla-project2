3acac126d28911f021fedb6e2744ac02ed96a939
Stephen Pellegrini <spellegrini156@gmail.com>
2018-09-08 19:26:51 -0700

    created models; added db.sync to bin/www

---
+++ models/article.js
@@ -0,0 +1,17 @@
+module.exports = function(sequelize, DataTypes) {
+    var Article = sequelize.define("Article", {
+        id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false},
+        title: {type: DataTypes.STRING, allowNull: false},
+        description: {type: DataTypes.TEXT, allowNull: false},
+        image: {type: DataTypes.STRING, allowNull: true},
+        cagetory: {type: DataTypes.STRING, allowNull: false}
+    });
+    Article.associate = function(models) {
+        Article.belongsTo(models.Recommendation, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+    return Article;
+}
