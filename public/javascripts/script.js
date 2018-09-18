bb837a7911d6585141b4677a85f8c9941e2fdfc3
Imen Mahjoubi <imenmahjoubi@Imens-MacBook-Pro.local>
2018-09-17 18:35:28 -0700

    stuff

---
+++ public/javascripts/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+    var user = {
+        first_name : '',
+        last_name : '',
+        gender : '',
+        email : '',
+    };
+    var count = 1;
+   /* var  getInputVal = function (input , val){
+        var input = '#'+input;
+            console.log( $('input').val());
+            var  val = $('input').val();
+            console.log(val);
+    };*/
+    $('#first_name').blur(function () {
+        console.log('first', $('#first_name').val());
+        var first = $('#first_name').val();
+        user.first_name = first;
+    });
+    $('#last_name').blur(function () {
+        console.log('last', $('#last_name').val());
+        var  last = $('#last_name').val();
+        user.last_name = last;
+    });
+    $('#female').click(function () {
+        console.log('female');
+        $('#male').hide();
+        $('.m').hide();
+        user.gender = 'female;'
+    });
+    $('#male').click(function () {
+        console.log('male');
+        $('#female').hide();
+        $('.f').hide();
+        user.gender = 'male;'
+    });
+    $('#bday').blur(function () {
+        console.log('bday', $('#bday').val());
+        user.birthday = $('#bday').val();
+    });
+
+   // if($('.submit').)
+
+
+});
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,44 @@
 var express = require('express');
-var router = express.Router();
+//var router = express.Router();
+var exphbs = require("express-handlebars");
+var path = require('path');
+var bodyParser = require('body-parser');
+var app = express();
+
+
+
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var tst ={
+    title: 'nutirition',
+    count : 1
+};
+var count = 1;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+app.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
 res.sendFile('../public/index.html');
 
 });
-router.get('/quiz', function(req, res) {
-    res.render('index', { title: 'MyNutriadvisor' });
+
+app.get('/quiz', function(req, res) {
+  // res.sendFile(path.resolve('../public/q1.html'));
+    res.render("q1",tst);
+
+});
+app.post('/quiz', function(req, res) {
+    // res.sendFile(path.resolve('../public/q1.html'));
+     tst.count++;
+     var count= "q"+tst.count;
+     console.log('question: ',tst.count);
+     console.log('data: ',req.body);
+    res.render(count,tst);
 
 
 });
 
-module.exports = router;
+
+module.exports = app;
